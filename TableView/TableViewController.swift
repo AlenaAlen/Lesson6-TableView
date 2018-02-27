13b1da1c9e9ff509adc2517552774ed886e10aa9
Alena_Kr <alenak_2708@mail.ru>
2018-02-27 22:25:10 +0500

    Add file

---
+++ TableView/TableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  TableViewController.swift
+//  TableView
+//
+//  Created by Mac on 27.02.18.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var nameArray = ["Аня","Маша","Саша","Олег","Иван","Ольга","София","Марина","Николай"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return nameArray.count
+    }
+
+  
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = nameArray[indexPath.row]
+        // Configure the cell...
+
+        return cell
+    }
+
+
+}
